f90163d4fdcb9f87583d14745d2fc0f0ca3ede1a
oktavianustjoea <ata.chai10@gmail.com>
2022-02-13 19:35:37 +0700

    settings: to add config for "STATICFILES_DIRS"

--- exercise/settings.py
+++ exercise/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# A list of locations of additional static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
