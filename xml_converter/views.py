97317aa0c7bde2605114e46fb1b5c66f782944b8
oktavianustjoea <ata.chai10@gmail.com>
2022-02-13 19:36:52 +0700

    views: to convert uploaded xml to json

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -1,10 +1,13 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from xml_converter import utils
 
 
 def upload_page(request):
-    if request.method == 'POST':
-        # TODO: Convert the submitted XML file into a JSON object and return to the user.
-        return JsonResponse({})
-
+    if request.method == 'POST' and request.FILES.get('file'):
+        result = {}
+        uploaded_file = request.FILES.get('file')
+        if uploaded_file:
+            result = utils.xml_parse(uploaded_file.read())
+        return JsonResponse(result)
     return render(request, "upload_page.html")
